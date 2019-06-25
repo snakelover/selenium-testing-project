34e39cfcbff0705fecd92fc41d15bd07d237eed5
Savva Morozov <pythontheback@gmail.com>
2019-06-25 23:32:24 +0300

    Add new method in MainPage and new test function

--- pages/main_page.py
+++ pages/main_page.py
@@ -6,3 +6,6 @@ class MainPage(BasePage):
     def go_to_login_page(self):
         link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
         link.click()
+
+    def should_be_login_link(self):
+        self.browser.find_element(By.CSS_SELECTOR, "#login_link_invalid")
