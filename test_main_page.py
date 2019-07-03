8a0b135ddaaf05da0fe67afe563aeb61b1a931af
Savva Morozov <pythontheback@gmail.com>
2019-07-03 21:36:04 +0300

    Move one test to test_product_page and mark tests

--- test_main_page.py
+++ test_main_page.py
@@ -27,11 +27,3 @@ def test_guest_cant_see_product_in_cart_opened_from_main_page(browser):
     page.go_to_cart_page()
     cart_page = CartPage(browser, browser.current_url)
     cart_page.should_see_no_products_in_the_cart()
-
-def test_guest_cant_see_product_in_cart_opened_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_cart_page()
-    cart_page = CartPage(browser, browser.current_url)
-    cart_page.should_see_message_no_products_in_the_cart()
--- test_product_page.py
+++ test_product_page.py
@@ -2,18 +2,10 @@ import time
 import pytest
 from pages.login_page import LoginPage
 from pages.product_page import ProductPage
+from pages.cart_page import CartPage
 
 
-# @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
-#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
-#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+@pytest.mark.need_review
 def test_guest_can_add_product_to_cart(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
     page = ProductPage(browser, link)
@@ -33,12 +25,22 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
     page.should_be_login_link()
-    
+
+@pytest.mark.need_review    
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
+
+@pytest.mark.need_review    
+def test_guest_cant_see_product_in_cart_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_cart_page()
+    cart_page = CartPage(browser, browser.current_url)
+    cart_page.should_see_message_no_products_in_the_cart()
 
 @pytest.mark.user_add_to_cart
 class TestUserAddToCartFromProductPage(object):
@@ -52,7 +54,8 @@ class TestUserAddToCartFromProductPage(object):
         page.register_new_user(email, password)
         page.should_be_authorized_user()
         self.browser = browser
-        
+
+    @pytest.mark.need_review 
     def test_user_can_add_product_to_cart(self):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
         page = ProductPage(self.browser, link)
