7904f51890efadc9972674bfd0bcc0b778578fff
Savva Morozov <pythontheback@gmail.com>
2019-07-03 21:17:33 +0300

    Delete unnecessary tests, save browser in variable

--- test_product_page.py
+++ test_product_page.py
@@ -4,44 +4,29 @@ from pages.login_page import LoginPage
 from pages.product_page import ProductPage
 
 
-@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
-def test_guest_can_add_product_to_cart(browser, link):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+# @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+def test_guest_can_add_product_to_cart(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
     page = ProductPage(browser, link)
     page.open()
     page.add_to_the_cart()
-    page.solve_quiz_and_get_code()
     page.should_be_success_of_addition_message()
     page.should_be_the_right_price_of_the_cart()
-
-# def test_guest_cant_see_success_message_after_adding_product_to_cart(browser):
-#     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
-#     page = ProductPage(browser, link)
-#     page.open()
-#     page.add_to_the_cart()
-#     page.should_not_be_success_message()
 
 def test_guest_cant_see_success_message(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
     page = ProductPage(browser, link)
     page.open()
     page.should_not_be_success_message()
-
-# def test_message_dissapeared_after_adding_product_to_cart(browser):
-#     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
-#     page = ProductPage(browser, link)
-#     page.open()
-#     page.add_to_the_cart()
-#     page.should_be_dissapeared()
 
 def test_guest_should_see_login_link_on_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
@@ -66,19 +51,19 @@ class TestUserAddToCartFromProductPage(object):
         password = "sword123pas"
         page.register_new_user(email, password)
         page.should_be_authorized_user()
-        time.sleep(5)
+        self.browser = browser
         
-    def test_user_can_add_product_to_cart(self, browser):
+    def test_user_can_add_product_to_cart(self):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
-        page = ProductPage(browser, link)
+        page = ProductPage(self.browser, link)
         page.open()
         page.add_to_the_cart()
         page.should_be_success_of_addition_message()
         page.should_be_the_right_price_of_the_cart()
 
-    def test_user_cant_see_success_message(self, browser):
+    def test_user_cant_see_success_message(self):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
-        page = ProductPage(browser, link)
+        page = ProductPage(self.browser, link)
         page.open()
         page.should_not_be_success_message()
 
